8ac105c4291fd5915536ae289c2a7cd15be6bc99
Piotr-PC\Piotr <pkubon2@gmail.com>
2018-01-04 14:42:40 +0100

    update Interfaces (you made a typo), added helpers(simple versione), added testproject

---
+++ FacebookBot/Helpers/StringHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Interfaces;
+
+namespace Helpers
+{
+    public static class StringHelper
+    {
+        public static List<Uri> GetUriToFriends(this string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DateTime GetBirthDateofUser(this string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetSexOfUse(this string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetUserResidence(this string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FacebookBot/TestProject/HelpersTestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class HelpersTestClass
+    {
+        [TestMethod]
+        public void GetUserBirthDayTest()
+        {
+            var data = "document".GetBirthDateofUser();
+        }
+        [TestMethod]
+        public void GetUserSexTest()
+        {
+            var data = "document".GetSexOfUse();
+        }
+        [TestMethod]
+        public void GetUserFriendsListTest()
+        {
+            var data = "document".GetUriToFriends();
+        }
+        [TestMethod]
+        public void GetUserResidenceTest()
+        {
+            var data = "document".GetUserResidence();
+        }
+    }
+}
